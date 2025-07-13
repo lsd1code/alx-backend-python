525c0d28dbcae248590a762c50a65ea11860884b
lesedi thibile <lesedi.xys11@gmail.com>
2025-07-13 15:45:53 +0200

    FEAT: reusable query context manager

--- python-context-async-perations-0x02/1-execute.py
+++ python-context-async-perations-0x02/1-execute.py
@@ -10,9 +10,54 @@ Instructions:
     Ensure to use the__enter__() and the __exit__() methods
 """
 
+from psycopg2 import connect, DatabaseError
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+class ExecuteQuery:
+    def __init__(self, query: str, param: int) -> None:
+        self.query = query
+        self.param = param
+        self.conn = None
+
+    def __enter__(self):
+        try:
+            USER = os.getenv("USER")
+            PASSWORD = os.getenv("PASSWORD")
+            DATABASE = os.getenv("DATABASE")
+            PORT = os.getenv("PORT")
+
+            if not self.conn:
+                self.conn = connect(dbname=DATABASE, user=USER,
+                                    password=PASSWORD, port=PORT)
+
+            with self.conn.cursor() as cursor:
+                cursor.execute(self.query, (self.param, ))
+                rows = cursor.fetchall()
+
+                if rows:
+                    return rows
+        except DatabaseError as db_e:
+            print(f'Exception: {db_e}')
+            raise db_e
+
+    def __exit__(self, exc_type, exec_val, exec_tb):  # type: ignore
+        if self.conn:
+            self.conn.close()
+            print(self.conn)
+
 
 def main():
-    pass
+    QUERY = "SELECT * FROM user_data WHERE age > 25;"
+    PARAM = 25
+
+    with ExecuteQuery(query=QUERY, param=PARAM) as result:
+        if result:
+            for row in result:
+                print(row)
 
 
 if __name__ == "__main__":
