e480f6ed101c5b3e9c5050f03cec0724bcba97c5
lesedi thibile <lesedi.xys11@gmail.com>
2025-07-13 15:08:03 +0200

    FEAT: custom class based context manager for Database connection

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,59 @@
+"""
+#! Custom class based context manager for Database connection
+
+#? Objective: create a class based context manager to handle opening and closing database connections automatically
+
+Instructions:
+
+    Write a class custom context manager DatabaseConnection using the __enter__ and the __exit__ methods
+
+    Use the context manager with the with statement to be able to perform the query SELECT * FROM users. Print the results from the query.
+"""
+
+from typing import Any
+# from psycopg2.extensions import connection
+from psycopg2 import connect, DatabaseError
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+class DatabaseConnection:
+    def __init__(self) -> None:
+        self.conn = None  # type: ignore
+
+    def __enter__(self):
+        try:
+            USER = os.getenv("USER")
+            PASSWORD = os.getenv("PASSWORD")
+            DATABASE = os.getenv("DATABASE")
+            PORT = os.getenv("PORT")
+
+            self.conn = connect(dbname=DATABASE, user=USER,
+                                password=PASSWORD, port=PORT)
+            return self.conn
+        except DatabaseError as db_e:
+            print(f'Could not connect to the Database. Please try again!')
+            raise db_e
+
+    def __exit__(self, exc_type, exc_value, exc_tb):  # type:ignore
+        if self.conn:
+            self.conn.close()
+
+
+def main():
+    with DatabaseConnection() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT * FROM user_data;")
+            rows = cursor.fetchall()
+
+            if rows:
+                for r in rows:
+                    print(r)
+
+        print(conn)
+
+
+if __name__ == "__main__":
+    main()
---
+++ python-context-async-perations-0x02/1-execute.py
@@ -0,0 +1,19 @@
+"""
+#! Reusable Query Context Manager
+
+#? Objective: create a reusable context manager that takes a query as input and executes it, managing both connection and the query execution
+
+Instructions:
+
+    Implement a class based custom context manager ExecuteQuery that takes the query: ”SELECT * FROM users WHERE age > ?” and the parameter 25 and returns the result of the query
+
+    Ensure to use the__enter__() and the __exit__() methods
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
