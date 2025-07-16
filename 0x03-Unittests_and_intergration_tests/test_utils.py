1d8b75d2aae0464e19f13afceadcf476df8ddad3
lesedi thibile <lesedi.xys11@gmail.com>
2025-07-16 23:04:57 +0200

    FEAT: parameterize a unit test

---
+++ 0x03-Unittests_and_intergration_tests/test_utils.py
@@ -0,0 +1,30 @@
+"""
+#! Parameterize a unit test
+
+#? Create a TestAccessNestedMap class that inherits from unittest.TestCase
+#? Implement the TestAccessNestedMap.test_access_nested_map() method to test that the method returns what is supposed to.
+#? Decorate the method with @parameterized.expand to test the function for following inputs:
+    >>> nested_map, path = ({'a': 1}, ['a'])
+    >>> nested_map, path = ({'a': {'b': 2}}, ['a',])
+    >>> nested_map, path = ({'a': {'b': 2}}, ['a', 'b'])
+"""
+
+from typing import Any
+import unittest
+from utils import access_nested_map
+from parameterized import parameterized
+
+
+class TestAccessNestedMap(unittest.TestCase):
+
+    @parameterized.expand([  # type:ignore
+        ({'a': 1}, ['a'], 1),
+        ({'a': {'b': 2}}, ['a',], {'b': 2}),
+        ({'a': {'b': 2}}, ['a', 'b'], 2)
+    ])
+    def test_access_nested_map(self, nested_map: dict[str, Any], path: list[str], expected: int):
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
