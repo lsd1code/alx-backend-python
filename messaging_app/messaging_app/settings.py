c879326847178a7d12d526e29d6d089e93c01c3a
Lesedi Thibile <lesedi.xys11@gmail.com>
2025-07-19 19:09:03 +0200

    FEAT: project setup

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -40,6 +40,15 @@ INSTALLED_APPS = [
     'chats'
 ]
 
+#! Global settings for REST API 
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
