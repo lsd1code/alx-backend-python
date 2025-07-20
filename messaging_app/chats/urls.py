08fd720122c89375ef80e07a1b7993de15ab6136
lesedi thibile <lesedi.xys11@gmail.com>
2025-07-20 17:10:41 +0200

    FEAT: Build api endpoints with views

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -3,14 +3,16 @@ from .views import index, UserViewSet, ConversationViewSet, MessageViewSet
 
 from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
-    path('', index, name="index"),
-    # path('users', UserViewSet.as_view(), name='users')
-]
-
 routers = DefaultRouter()
 routers.register('conversation', ConversationViewSet, basename="conversations")
 routers.register('user', UserViewSet, basename="user")
 routers.register('message', MessageViewSet, basename="message")
 
-urlpatterns += router.urls
+
+urlpatterns = [
+    path('', index, name="index"),
+    # path('users', UserViewSet.as_view(), name='users')
+]
+
+
+urlpatterns += routers.urls
