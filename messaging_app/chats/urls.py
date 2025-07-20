f69970613a8bf2eb2a37d0c55b00577cd1fd16f0
lesedi thibile <lesedi.xys11@gmail.com>
2025-07-20 17:08:42 +0200

    FEAT: Build api endpoints with views

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from .views import index, UserViewSet, ConversationViewSet, MessageViewSet
 
 from rest_framework.routers import DefaultRouter
