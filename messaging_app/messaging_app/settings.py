4490b9acc04232ca93f46f816f21a8c2de3009ae
lesedi thibile <lesedi.xys11@gmail.com>
2025-07-27 16:43:25 +0200

    FEAT: pagination and filtering

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -145,17 +145,14 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticated",
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
-            'rest_framework.authentication.BasicAuthentication',
-            'rest_framework_simplejwt.authentication.JWTAuthentication',
-            'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
     ],
-    
-    'DEFAULT_PAGINATION_CLASSES': [
-        'rest_framework.pagination.PageNumberPagination',
-        'rest_framework.pagination.LimitOffsetPagination',
-    ],
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20
 }
