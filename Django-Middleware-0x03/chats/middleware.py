4defed86c280fecf4bdb9b4d93b13b082b4a25ae
lesedi thibile <lesedi.xys11@gmail.com>
2025-07-27 22:41:55 +0200

    FEAT: restrict chat by access time

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,6 +1,8 @@
 import time
+import threading
 from datetime import datetime
 from django.http import HttpResponseForbidden
+
 
 class RequestLoggingMiddleware:
     def __init__(self, get_response):
@@ -36,3 +38,43 @@ class RestrictAccessByTimeMiddleware:
         response = self.get_response(request)
 
         return response
+
+
+class OffensiveLanguageMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.number_of_messages = 0
+        self.timer = self.timer_function()
+
+    def __call__(self, request):
+        client_ip = self.get_client_ip(request)
+
+        if self.number_of_messages >= 5 and not self.timer.is_alive():
+            return HttpResponseForbidden("You cannot send more than 5 messages per minute")
+
+        self.number_of_messages += 1
+        response = self.get_response(request)
+
+        return response
+    
+    def timer_function(self):
+        print("timer_start")
+
+        def timer_complete():
+            pass
+            # timer.start()
+
+        timer = threading.Timer(60.0, timer_complete)
+        timer.start()
+
+        return timer
+
+    def get_client_ip(self, request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(",")[0].strip()
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+
+        return ip
