81203b66ef09199149e6972f8885594f82f396ea
lesedi <lsd1code@gmail.com>
2025-08-10 22:15:28 +0200

    FEAT: setup up a docker environment

--- Django-signals_orm-0x04/messaging/managers.py
+++ Django-signals_orm-0x04/messaging/managers.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from django.db import models
 from django.shortcuts import get_object_or_404
 
@@ -8,3 +9,17 @@ class UnreadMessagesManager(models.Manager):
 
     def unread_for_user(self, user_id):
         return self.get_queryset().filter(sender__id=2)
+=======
+from django.db import models
+from django.shortcuts import get_object_or_404
+
+# from .models import User
+
+
+class UnreadMessagesManager(models.Manager):
+    def unread(self):
+        return self.get_queryset().filter(unread=True)
+
+    def unread_for_user(self, user_id):
+        return self.get_queryset().filter(sender=user_id)
+>>>>>>> e696047 (FEAT: setup a docker environment)
--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from .models import User, Message
 
 from django.shortcuts import get_object_or_404
@@ -79,3 +80,84 @@ def delete_user(request: Request, user_id: int):
     user.delete()
 
     return Response("User deleted successfully", status.HTTP_204_NO_CONTENT)
+=======
+from .models import User, Message
+
+from django.shortcuts import get_object_or_404
+
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.request import Request
+
+
+@api_view(['GET'])
+def message_list(request: Request):
+    messages = Message.objects.all()
+    return Response(f'Total Messages: {len(messages)}')
+
+
+@api_view(['GET'])
+def unread_message_list(request: Request):
+    m = Message.unread.unread_for_user(3).only()
+    return Response(f'Unread messages: {len(m)}')
+
+
+@api_view(['GET'])
+def message(request: Request, message_id: int):
+    message = Message.objects.filter(
+        pk=message_id
+    ).select_related().prefetch_related('replies').first()
+
+    if not message:
+        return Response('404 Message Not Found', status.HTTP_404_NOT_FOUND)
+
+    replies = message.replies.all()
+    return Response(f'Message: {message.content}, replies: {len(replies)}')
+
+
+@api_view(['POST'])
+def create_message(request: Request):
+    sender = get_object_or_404(
+        User, pk=request.data.get('sender'))
+    receiver = get_object_or_404(
+        User, pk=request.data.get('receiver'))
+    content = request.data.get('content')
+
+    if len(str(content)) < 1:
+        return Response('Message content cannot be empty', status.HTTP_204_NO_CONTENT)
+
+    Message.objects.create(sender=sender, receiver=receiver, content=content)
+    return Response(f'Message created', status.HTTP_201_CREATED)
+
+
+@api_view(['POST'])
+def reply_message(request: Request, message_id: int):
+    parent_message = get_object_or_404(Message, pk=message_id)
+    reply_sender = get_object_or_404(
+        User, pk=request.data['sender'])
+    reply_receiver = parent_message.sender
+
+    Message.objects.create(
+        # sender=reply_sender,
+        sender=request.user,
+        receiver=reply_receiver,
+        parent_message=parent_message,
+        content=request.data['content']
+    )
+
+    return Response(f'Message {parent_message.content}', status.HTTP_201_CREATED)
+
+
+@api_view(['DELETE'])
+def delete_user(request: Request, user_id: int):
+    '''
+    Implement a post_delete signal on the User model to delete all messages, notifications, and message histories associated with the user.
+
+    Ensure that foreign key constraints are respected during the deletion process by using CASCADE or custom signal logic.
+    '''
+    user = get_object_or_404(User, pk=user_id)
+    user.delete()
+
+    return Response('User deleted successfully', status.HTTP_204_NO_CONTENT)
+>>>>>>> e696047 (FEAT: setup a docker environment)
